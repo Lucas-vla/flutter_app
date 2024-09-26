61fb0a07749cf1653caf5c57c73f9a3c19803d8e
Lucas <lucasviola.pro@hotmail.com>
2024-09-26 09:58:39 +0200

    affichage resultat exo2

---
+++ exo2/lib/components.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:exo2/consts.dart';
+
+class MyPadding extends Padding {
+  const MyPadding({super.key, required super.child}): super(padding: defaultPadding);
+}
+
+class MyText extends Text {
+  const MyText(super.data, {super.key}): super(style: defaultTextStyle);
+}
+
+class MySizedBox extends SizedBox {
+  const MySizedBox({super.key, required super.child}): super(width: 100);
+}
--- exo2/lib/main.dart
+++ exo2/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:exo2/pages/results.dart';
 import 'package:flutter/material.dart';
+import 'package:exo2/consts.dart';
+import 'package:exo2/components.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,32 +35,95 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _formKey = GlobalKey<FormState>();
+  late double _value1;
+  late double _value2;
+
+  _displayResult(String operation) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => ResultsPage(operation)));
+  }
+
+  String? _operandValidator(value) {
+    if (value == null ||
+        value.trim().isEmpty ||
+        double.tryParse(value) == null) {
+      return "veuillez saisir un nombre";
+    }
+    return null;
+  }
+
+  _add() {
+    if (!_formKey.currentState!.validate()) return;
+    _formKey.currentState?.save();
+    _displayResult("$_value1 + $_value2 = ${_value1 + _value2}");
+  }
+
+  _sub() {
+    if (!_formKey.currentState!.validate()) return;
+    _formKey.currentState?.save();
+    _displayResult("$_value1 - $_value2 = ${_value1 - _value2}");
+  }
+
+  _mul() {
+    if (!_formKey.currentState!.validate()) return;
+    _formKey.currentState?.save();
+    _displayResult("$_value1 * $_value2 = ${_value1 * _value2}");
+  }
+
+  _div() {
+    if (!_formKey.currentState!.validate()) return;
+    _formKey.currentState?.save();
+    _displayResult("$_value1 / $_value2 = ${_value1 / _value2}");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Theme
-              .of(context)
-              .colorScheme
-              .inversePrimary,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
         body: Form(
             key: _formKey,
-            child: Column(
+            child: Column(children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                SizedBox(
+                  width: 200,
+                  child: TextFormField(
+                    onSaved: (value) =>
+                        _value1 = double.parse(value.toString()),
+                    validator: _operandValidator,
+                    style: defaultTextStyle,
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                SizedBox(
+                    width: 200,
+                    child: TextFormField(
+                      onSaved: (value) =>
+                          _value2 = double.parse(value.toString()),
+                      validator: _operandValidator,
+                      style: defaultTextStyle,
+                      keyboardType: TextInputType.number,
+                    ))
+              ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      SizedBox(
-
-                      )
-                    ],
-                  )
-                ]
-            )
-        )
-    );
+                  MySizedBox(
+                      child:
+                          ElevatedButton(onPressed: _add, child: MyText("+"))),
+                  MySizedBox(
+                      child:
+                          ElevatedButton(onPressed: _sub, child: MyText("-"))),
+                  MySizedBox(
+                      child:
+                          ElevatedButton(onPressed: _mul, child: MyText("*"))),
+                  MySizedBox(
+                      child:
+                          ElevatedButton(onPressed: _div, child: MyText("/"))),
+                ],
+              )
+            ])));
   }
 }
